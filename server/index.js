0e229ea34358f341fa34ed6b5e620e4698f3603f
ivarmlee11 <ivarr5@gmail.com>
2019-07-15 18:52:12 -0700

    About to create hero cards

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const robot = require('robotjs');
+const characters = require('./characters.js');
+
+let ticker = 0;
+
+app.get('/characters/:resolution/:character', (req, res) => {
+    let character = req.params.character;
+    let desktopRes = req.params.resolution;
+    setTimeout(() => { 
+        while (ticker < 500) {
+            robot.moveMouse(characters[character][desktopRes].x, characters[character][desktopRes].y)
+            robot.mouseClick('left');
+            ticker += 1;
+        }
+        ticker = 0;
+        console.log('Hero locked and loaded.');
+    }, 1000);
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ src/components/HeroCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HeroCards.css';
+
+function HeroCard() {
+  return (
+    <div className="selection-screen">
+        Hero Card!
+    </div>
+  );
+}
+
+export default HeroCard;
